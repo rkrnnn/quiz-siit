9e47db393dd6463d0e56a608a654ba932f6121ee
Cristiana <calin.cristiana.e@gmail.com>
2021-06-23 09:17:08 +0300

    Added JS to Add New Quesiton modal

--- js/addQuestion.js
+++ js/addQuestion.js
@@ -1,8 +1,47 @@
 console.log('addQuestions.js');
 
 var newQuestionTextInput = document.querySelector(".add-q");
-var answer01Input = document.querySelector(".answ01");
+var answersInput = document.querySelectorAll(".modal-answers .input-grp");
+var addQuestionBtn = document.querySelector(".modal-footer .btn-generic");
+var anwersIncompleteAlert = document.querySelector(".answers-incomplete");
 
 function addQuestion() {
-    console.log('Questions added.');
+    var answers = [];
+    var atLeastOneCorrectAnsw = false;
+
+    var i = 0;
+    while (i < 4) {
+        answers[i] = {
+            text: answersInput[i].firstChild.nextSibling.value,
+            right: Number(answersInput[i].firstChild.nextSibling.nextSibling.nextSibling.checked)
+        };
+
+        if (answers[i].right) {
+            atLeastOneCorrectAnsw = true;
+        }
+
+        i++;
+    }
+
+    console.log(answers);
+
+    if (!atLeastOneCorrectAnsw) {
+        var i = 0;
+        while (i < 4) {
+            answersInput[i].firstChild.nextSibling.nextSibling.nextSibling.classList.add('is-invalid')
+            i++;
+        };
+
+        anwersIncompleteAlert.style.display = '';
+    }
+    else {
+        var i = 0;
+        while (i < 4) {
+            answersInput[i].firstChild.nextSibling.nextSibling.nextSibling.classList.remove('is-invalid')
+            i++;
+        };
+
+        anwersIncompleteAlert.style.display = 'none';
+    }
+
 }
